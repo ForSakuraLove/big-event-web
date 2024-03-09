37e8888f315429a2d34316e0d87ec9d80c75833b
zwk <2076637688@qq.com>
2024-03-09 10:36:42 +0800

    Add systemManage routes

--- src/router/index.js
+++ src/router/index.js
@@ -7,21 +7,42 @@ import ArticleManageVue from '@/views/article/ArticleManage.vue'
 import UserAvatarVue from '@/views/user/UserAvatar.vue'
 import UserInfoVue from '@/views/user/UserInfo.vue'
 import UserResetPasswordVue from '@/views/user/UserResetPassword.vue'
+import UserManageVue from '@/views/systemManage/UserManage.vue'
+import RoleManageVue from '@/views/systemManage/RoleManage.vue'
+import MenuManageVue from '@/views/systemManage/MenuManage.vue'
 
 const routes = [
     { path: '/login', component: LoginVue },
     {
         path: '/',
         component: LayoutVue,
-        redirect: '/article/category',
+        redirect: 'article/category',
         children: [
-            { path: '/article/category', component: ArticleCategoryVue },
-            { path: '/article/manage', component: ArticleManageVue },
-            { path: '/user/avatar', component: UserAvatarVue },
-            { path: '/user/info', component: UserInfoVue },
-            { path: '/user/resetPassword', component: UserResetPasswordVue },
+            { path: 'article/category', component: ArticleCategoryVue },
+            { path: 'article/manage', component: ArticleManageVue },
         ]
-    }
+    },
+    {
+        path: '/user',
+        component: LayoutVue,
+        redirect: '/avatar',
+        children: [
+            { path: 'avatar', component: UserAvatarVue },
+            { path: 'info', component: UserInfoVue },
+            { path: 'resetPassword', component: UserResetPasswordVue },
+        ]
+    },
+    {
+        path: '/systemManage',
+        component: LayoutVue,
+        redirect: '/userManage',
+        children: [
+            { path: 'userManage', component: UserManageVue },
+            { path: 'roleManage', component: RoleManageVue },
+            { path: 'menuManage', component: MenuManageVue },
+        ]
+    },
+
 ]
 
 const router = createRouter({
